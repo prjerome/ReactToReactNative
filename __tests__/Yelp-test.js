9110cfa920a67e8941f5c4e29033c9fa3c7e4cec
prjerome <prjeorme@gmail.com>
2020-12-08 15:44:36 +0100

    Continuing TDD on Yelp.js

--- __tests__/Yelp-test.js
+++ __tests__/Yelp-test.js
@@ -1,11 +1,50 @@
 import {Yelp, apiKey} from '../src/util/Yelp';
 
 it('given there is Yelp.search method, when it is called, it returns a truthy value', () => {
-  expect(Yelp.search()).toEqual(true);
+  expect(Yelp.search('pizza', 'munich', 'best_match')).toBeTruthy();
 });
 
 it('given there is a apiKey variable, it is truthy', () => {
   expect(apiKey).toBeTruthy();
 });
 
+it('given there is a fetch call, it is truthy', () => {
+  //Arrange
+  const queryString = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${'pizza'}&location=${'munich'}&sort_by=${'best_match'}`;
 
+  //Act
+
+  const fetchResponse = () => {
+    return fetch(queryString, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+  };
+
+  //Assert
+  expect(fetchResponse).toBeTruthy();
+});
+
+/*it('given there is a fetch response, when response.json is called, the return value is not null or undefined', () => {
+  //Arrange
+  const queryString = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${'pizza'}&location=${'munich'}&sort_by=${'best_match'}`;
+
+  //Act
+
+  const fetchResponse = () => {
+    return fetch(queryString, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
+  };
+
+  //Assert
+  expect(
+    fetchResponse.then((response) => {
+      return response.json();
+    })
+  ).anything();
+});
+*/
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,7 +2,13 @@ export const apiKey =
   'k1mfKLNEZjzoeDTyDfRc8BCDz45ELrIKTIKOf4PGiKr6NCP3ePK1ef4Ic_H4nUXsMiimA5V00gBXyMoWHvM8ZOVZkK9jVdNeyAcdn6WUsWYlBJ4xOQam2k4f-M8CX3Yx';
 
 export const Yelp = {
-  search() {
-    return true;
+  search(term, location, sortBy) {
+    const queryString = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+
+    return fetch(queryString, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    });
   },
 };
