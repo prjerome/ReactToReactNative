d24d41056359844448ab7f82a074c08cfb96d7fe
Pradeep Jerome <prjerome@gmail.com>
2021-01-03 13:30:22 +0100

    Removed API key

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,5 +1,5 @@
 export const apiKey =
-  'k1mfKLNEZjzoeDTyDfRc8BCDz45ELrIKTIKOf4PGiKr6NCP3ePK1ef4Ic_H4nUXsMiimA5V00gBXyMoWHvM8ZOVZkK9jVdNeyAcdn6WUsWYlBJ4xOQam2k4f-M8CX3Yx';
+  '';
 
 export const Yelp = {
   search(term, location, sortBy) {
